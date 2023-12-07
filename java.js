64ad3a380b66a03f0a267884e80ae5520319949c
Manutencaolondon <152867490+Manutencaolondon@users.noreply.github.com>
2023-12-07 05:36:46 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,36 +1,3 @@
-  
-const data=new Date()
-const dia=data.getDay()
-const mes=data.getMonth()+1
-const ano=data.getFullYear()
-const hora=data.getHours()
-const minutos=data.getMinutes()
-const segundos=data.getSeconds()
-document.getElementById("Hora").innerHTML="hora certa:"+hora+":"+minutos+":"+segundos
-if(hora<10){Hora.innerHTML="Hora que O.S foi Gerada"+"0"+hora+":"
-    }
-else{Hora.innerHTML="Hora que O.S foi Gerada"+hora+":"}
-
-if(minutos<10){Minutos.innerHTML="0"+minutos+":"
-    
-}
-else{Minutos.innerHTML=minutos+":"}
-
-
-if(segundos<10){Segundos.innerHTML="0"+segundos
-    
-}
-else{Segundos.innerHTML=segundos}
-
-document.getElementById("Dt").innerHTML="-"+dia+"/"+mes+"/"+ano;
-if(dia<10) {Dt.innerHTML="-0"+dia+"/"+mes+"/"+ano;
-    
-}
-if(mes<10) {Dt.innerHTML="-"+dia+"/0"+mes+"/"+ano;
-    
-}
-
-  
    var signaturePad1 = new SignaturePad(document.getElementById('signatureCanvas1'));
         var signaturePad2 = new SignaturePad(document.getElementById('signatureCanvas2'));
 
@@ -67,6 +34,10 @@ if(mes<10) {Dt.innerHTML="-"+dia+"/0"+mes+"/"+ano;
         }
 
 
+
+
+
+
 document.getElementById('fileInput').addEventListener('change', handleFileSelect);
 
         function handleFileSelect(event) {
@@ -87,5 +58,3 @@ document.getElementById('fileInput').addEventListener('change', handleFileSelect
                 reader.readAsDataURL(file);
             }
         }
-
-               
